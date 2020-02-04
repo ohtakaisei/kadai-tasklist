51cca5f97333e3f09dc49c38bd0170f88cdec833
kaisei.ota <blogsenmonyou@gmail.com>
2020-02-04 07:16:09 +0000

    CRUD tasks

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のやること詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>やること</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+{!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+@endsection
