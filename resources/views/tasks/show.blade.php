97c0b33644411928935550258a088b117af33230
kaisei.ota <blogsenmonyou@gmail.com>
2020-02-04 07:44:48 +0000

    CRUD tasks

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -10,7 +10,7 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>id</th>
+            <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
     </table>
